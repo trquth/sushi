1adb2ca185431e89dcde5503b0602f6b17152070
trquth <tranquthien@gmail.com>
2016-06-15 05:22:06 +0700

    Build project

---
+++ SuShiSPA/SuShiSPA/App_Start/BundleConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SuShiSPA
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/sushicss").Include(
+                      "~/Content/css/style.css",
+                      "~/Content/css/kendo.common.min.css",
+                      "~/Content/css/kendo.rtl.min.css",
+                      "~/Content/css/kendo.metro.min.css"));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                     "~/Scripts/angular-1.5.6/angular.min.js",
+                     "~/Scripts/angular-1.5.6/angular-route.min.js",
+                     "~/Scripts/angular-1.5.6/angular-animate.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/kendo/2016.1.226/kendo.web.min.js",
+                "~/Scripts/kendo/2016.1.226/kendo.angular.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/sushijs").Include(
+                     "~/Content/js/sushi.js"));
+        }
+    }
+}
---
+++ SuShiSPA/SuShiSPA/Scripts/common.js
@@ -0,0 +1,86 @@
+﻿var Hcc = {
+    startAjax: function () {
+        $("body").addClass("loading");
+    },
+    endAjax: function () {
+        $("body").removeClass("loading");
+    },
+    isNullorWhiteSpace: function (str) {
+        if (!str)
+            return true;
+        if (str.length === 0)
+            return true;
+        if (/^\s*$/.test(str))
+            return true;
+        if (typeof (str) === "string" && !str.trim())
+            return true;
+        return false;
+    },
+    isNullorUndefined: function (obj) {
+        if(typeof (obj) !== "undefined" && obj !== null)
+            return false;
+        return true;
+    },
+    showInfo: function (msg) {
+       
+        $(".hcc-message-box .msg-box-title").html("<i class='fa fa-info-circle'></i> Thông báo");
+        $(".hcc-message-box .modal-body p").html(msg);
+        $(".hcc-message-box .modal-body #msg-body").removeClass("alert-success");
+        $(".hcc-message-box .modal-body #msg-body").removeClass("alert-danger");
+        $(".hcc-message-box .modal-body #msg-body").addClass("alert-success");
+        $(".hcc-message-box").modal("show");
+        window.setTimeout(function () { $(".hcc-message-box").modal("hide"); }, 3000);
+    },
+    showError: function (msg) {
+        $(".hcc-message-box .msg-box-title").html("<i class='fa fa-exclamation-triangle'></i> Thông báo lỗi");
+        $(".hcc-message-box .modal-body p").html(msg);
+        $(".hcc-message-box .modal-body #msg-body").removeClass("alert-success");
+        $(".hcc-message-box .modal-body #msg-body").removeClass("alert-danger");
+        $(".hcc-message-box .modal-body #msg-body").addClass("alert-danger");
+        $(".hcc-message-box").modal("show");
+    },
+    formatCurrency: function (nStr) {
+        nStr += '';
+        var x = nStr.split('.');
+        var x1 = x[0];
+        var x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        }
+        return x1 + x2;
+    }
+};
+
+$(document).ready(function () {
+    // file upload
+    $(".hcc-upload-btn").change(function () {
+        $(".hcc-upload-display-value").val($(this).val());
+    });
+
+    $('.modal').modal({
+        keyboard: false,
+        backdrop: 'static',
+        show: false
+    });
+    // Jquery draggable
+    $('.modal-dialog').draggable({
+        handle: ".modal-header"
+    });
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+    });
+    $("#btnLogout").click(function () {
+        $("#mdlLogout").modal("show");
+    });
+
+    setTimeout("$('#alert-panel').html('');", 3000);
+});
+
+$(function () {
+    //moment.locale('vi');
+    //$(".datetime-picker-control").datetimepicker({
+    //    format: "DD/MM/YYYY"
+    //});
+});
