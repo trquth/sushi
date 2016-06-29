787f0974c790bd0f75cbbf00393c1f32f71a17b9
trquth <tranquthien@gmail.com>
2016-06-29 16:51:23 +0700

    Finish--->In company

--- SuShiSPA/SuShiSPA/App/controllers/homeController.js
+++ SuShiSPA/SuShiSPA/App/controllers/homeController.js
@@ -1,12 +1,5 @@
-﻿sushiApp.controller("homeController", function ($scope, sushiFactory,product) {
-    var object =JSON.stringify({
-        "CallerKey": "Not Use Now",
-        "CommandId": -1,
-        "RequestParams":
-            []
-    })
-    jsonDataObj = JSON.stringify(object);
-    sushiFactory.getProducts(jsonDataObj).then(function (response) {
+﻿sushiApp.controller("homeController", function ($scope, sushiFactory) {
+    sushiFactory.getProducts().then(function (response) {
         $scope.products = response.data.ResponseData;        
     }, function (error) {       
         console.log("Error");
